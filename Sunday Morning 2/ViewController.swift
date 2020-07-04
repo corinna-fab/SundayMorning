aa8fce5ef2e77588bbac568c6189a4423303757e
Corinna Fabre <corinna.fabre@gmail.com>
2020-07-04 08:53:31 -0700

    Initial Commit

---
+++ Sunday Morning 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sunday Morning 2
+//
+//  Created by Corinna Fabre on 7/4/20.
+//  Copyright © 2020 Corinna Fabre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
