6ba160f5c4e799254baab3600489ac43c4d99ef9
Corinna Fabre <corinna.fabre@gmail.com>
2020-07-04 09:04:54 -0700

    Completed setup

---
+++ Sunday Morning 2/Controllers/PhotoViewerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoViewerViewController.swift
+//  Sunday Morning 2
+//
+//  Created by Corinna Fabre on 7/4/20.
+//  Copyright © 2020 Corinna Fabre. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
